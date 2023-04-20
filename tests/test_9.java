fece1038bdc9aec8b66c29b99f4eefed1217856b
Priyanka Jalan <prijalan2001@gmail.com>
2023-04-20 23:08:49 +0530

    Update test9

--- tests/test_9.java
+++ tests/test_9.java
@@ -3,9 +3,10 @@ class Factors{
   {
     int number = 60;
     for (int i = 1; i <= number; i++) {
-      if (number % i == 0) {
-        System.out.print(i);
-      }
+        if (number % i == 0) {
+          System.out.print(i);
+        }
     }
+    return 0;
   }
 }
